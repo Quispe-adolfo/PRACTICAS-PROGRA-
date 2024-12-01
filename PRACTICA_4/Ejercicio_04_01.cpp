e41c38d4aa8d03233f5503772a13c9d200aba3da
Quispe-adolfo <158525014+Quispe-adolfo@users.noreply.github.com>
2024-12-01 11:59:54 -0400

    codigo 1

--- PRACTICA_4/Ejercicio_04_01.cpp
+++ PRACTICA_4/Ejercicio_04_01.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 
-using namespace std:
+using namespace std;
 
 int  main()
 {
 
     cout << "hola mundo"<<endl;
-    system("pause")
+    system("pause");
     return 0;
 }
